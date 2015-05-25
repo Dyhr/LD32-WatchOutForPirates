1b4a380c2ab21155944a6f4eb2ec59287e0c2601
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2015-05-25 20:28:54 +0200

    Menu done for now

--- src/com/tinyprogress/spaceship/ui/Start.hx
+++ src/com/tinyprogress/spaceship/ui/Start.hx
@@ -12,6 +12,7 @@ import openfl.text.AntiAliasType;
 import openfl.text.TextField;
 import openfl.text.TextFieldAutoSize;
 import openfl.text.TextFormat;
+import openfl.text.TextFormatAlign;
 
 /**
  * ...
@@ -19,10 +20,12 @@ import openfl.text.TextFormat;
  */
 class Start extends Sprite
 {
-	private var format:TextFormat = new TextFormat("Comfortaa", 32, 0xDDBB77, true);
+	private var format:TextFormat = new TextFormat("Comfortaa", 32, 0xDDBB77, true,null,null,null,null,TextFormatAlign.CENTER);
 	private var title:TextField;
+	private var credits:TextField;
 	
 	private var buttons:Array<DisplayObject> = [];
+	private var back:DisplayObject;
 
 	public function new(main:Main) 
 	{
@@ -38,18 +41,49 @@ class Start extends Sprite
 		title.text = "Watch Out for Pirates";
 		addChild(title);
 		
+		credits = new TextField();
+		format.size = 20;
+		credits.embedFonts = true;
+		credits.defaultTextFormat = format;
+		credits.setTextFormat(format);
+		credits.autoSize = TextFieldAutoSize.CENTER;
+		credits.antiAliasType = AntiAliasType.ADVANCED;
+		credits.mouseEnabled = false;
+		credits.multiline = true;
+		credits.text = "Made by:\nRasmus Dyhr Larsen";
+		
 		var butt = create("Play", 24);
+		buttons.push(butt);
 		bind(butt, function(e:MouseEvent) {
 			main.start();
 		});
-		butt = create("Credits", 24);
+		butt = create("Controls (Pending)", 24);
+		buttons.push(butt);
 		bind(butt, function(e:MouseEvent) {
 			// TODO
 		});
+		butt = create("Credits", 24);
+		buttons.push(butt);
+		bind(butt, function(e:MouseEvent) {
+			removeChild(title);
+			for (button in buttons) removeChild(button);
+			addChild(back);
+			addChild(credits);
+		});
 		butt = create("Quit", 24);
+		buttons.push(butt);
 		bind(butt, function(e:MouseEvent) {
 			System.exit(0);
 		});
+		
+		back = create("Back", 24);
+		bind(back, function(e:MouseEvent) {
+			addChild(title);
+			for (button in buttons) addChild(button);
+			removeChild(back);
+			removeChild(credits);
+		});
+		removeChild(back);
 		
 		addEventListener(Event.ENTER_FRAME, update);
 		addEventListener(Event.REMOVED_FROM_STAGE, destroy);
@@ -75,7 +109,6 @@ class Start extends Sprite
 		
 		var button = new SimpleButton(textfield, textfield, textfield, hit);
 		addChild(button);
-		buttons.push(button);
 		return button;
 	}
 	private function bind(button:DisplayObject, click:Dynamic->Void) {
@@ -86,6 +119,8 @@ class Start extends Sprite
 	{
 		title.x = - title.width / 2 + stage.stageWidth/2;
 		title.y = stage.stageHeight / 4;
+		credits.x = - credits.width / 2 + stage.stageWidth/2;
+		credits.y = stage.stageHeight / 4;
 		
 		var pos = stage.stageHeight/4;
 		for (button in buttons) {
@@ -93,6 +128,9 @@ class Start extends Sprite
 			button.y = stage.stageHeight / 4 + title.height + pos;
 			pos += button.height + 6;
 		}
+		
+		back.x = - back.width / 2 + stage.stageWidth/2;
+		back.y = stage.stageHeight-(stage.stageHeight / 4+back.height);
 	}
 	
 	private function destroy(e:Event):Void 
